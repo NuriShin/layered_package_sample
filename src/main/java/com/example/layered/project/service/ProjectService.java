b3db1e957c5642f70ff9e3ee25c914a906f89130
NuriShin <nuri.mustaine@gmail.com>
2025-09-22 13:12:33 +0900

    Update ProjectService.java

--- src/main/java/com/example/layered/project/service/ProjectService.java
+++ src/main/java/com/example/layered/project/service/ProjectService.java
@@ -1,14 +1,16 @@
 package com.example.layered.project.service;
 
-import com.example.layered.project.dto.request.CreateProjectRequest;
-import com.example.layered.project.dto.request.SearchProjectCondition;
-import com.example.layered.project.dto.response.ProjectResponse;
+import com.example.layered.project.domain.Project;
+import com.example.layered.project.dto.request.*;
+import com.example.layered.project.dto.response.CreateProjectResponse;
+import com.example.layered.project.dto.response.SearchProjectResponse;
 import com.example.layered.project.mapper.ProjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -17,21 +19,39 @@ public class ProjectService {
 
     private final ProjectMapper projectMapper;
 
-    public Long create(CreateProjectRequest request) {
-        return projectMapper.insert(request.toProject());
+    // 단건 insert
+    @Transactional
+    public CreateProjectResponse create(CreateProjectRequest request) {
+
+        Project project = request.toProject();
+        projectMapper.insert(project);
+
+        return CreateProjectResponse.fromDomain(project);
     }
+
+    // 1 - nn 단위 insert
+    @Transactional
+    public List<CreateProjectResponse> createAll(List<CreateProjectRequest> request) {
+
+        return request.stream()
+                      .map(this::create).toList();
+
+    }
+
+    // nnn 단위 insert -> mapper 내부에서 foreach (update 참고)
+    // nnnn 단위 insert -> batch 사용
+
+    // 1 ~ nnn 단위 update 예시
+    @Transactional
+    public void update(UpdateProjectListRequest request) {
+        projectMapper.update(request.toProjects());
+    }
+
+    // 조회
+    public SearchProjectResponse findByCond(SearchProjectCondition searchProjectCondition) {
+        return projectMapper.select(searchProjectCondition);
+    }
+
     
-    public void update() {
-        // TODO: Implement approval status update logic
-    }
-
-    public List<ProjectResponse> findByIds(List<Integer> ids) {
-        return projectMapper.select(ids.stream()
-                            .map(id -> new SearchProjectCondition(id, null, -1L, null))
-                            .toList());
-    }
-
-    public void findByCond() {
-        // TODO: Implement approval status update logic
-    }
+   
 }
