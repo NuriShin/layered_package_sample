b8d9177eccc92b1e96ab46f441283579b54a10f6
NuriShin <nuri.mustaine@gmail.com>
2025-09-22 13:12:18 +0900

    Update ProjectMapper.java

--- src/main/java/com/example/layered/project/mapper/ProjectMapper.java
+++ src/main/java/com/example/layered/project/mapper/ProjectMapper.java
@@ -1,15 +1,17 @@
 package com.example.layered.project.mapper;
 
 import com.example.layered.project.domain.Project;
-import com.example.layered.project.dto.request.CreateProjectRequest;
 import com.example.layered.project.dto.request.SearchProjectCondition;
-import com.example.layered.project.dto.response.ProjectResponse;
+import com.example.layered.project.dto.response.SearchProjectResponse;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 @Mapper
 public interface ProjectMapper {
+    // Create - 단건 기준, 서비스에서 루프
     Long insert(Project project);
-    List<ProjectResponse> select(List<SearchProjectCondition> searchParam);
+    void update(List<Project> projects);
+    SearchProjectResponse select(SearchProjectCondition searchProjectCondition);
+    //List<ProjectResponse> select(List<SearchProjectCondition> searchParams);
 }
