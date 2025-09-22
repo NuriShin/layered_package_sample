d15fc53bfd211019b20eb7170a66eeddbc79c245
NuriShin <nuri.mustaine@gmail.com>
2025-09-22 13:09:13 +0900

    Update SearchProjectCondition.java

--- src/main/java/com/example/layered/project/dto/request/SearchProjectCondition.java
+++ src/main/java/com/example/layered/project/dto/request/SearchProjectCondition.java
@@ -5,9 +5,12 @@ import com.example.layered.project.domain.Project;
 public record SearchProjectCondition(
         int id
         , String name
-        , long pay
+        , long minPay
+        , long maxPay
         , String supervisorId) {
-
-
-
+    public SearchProjectCondition {
+        if (minPay > maxPay) {
+            throw new IllegalArgumentException("minPay는 maxPay보다 클 수 없습니다.");
+        }
+    }
 }
